a8c6e56cd9527c2ae8a9f066c204a8cbc91d774e
Christelle Nirere <mariechristellenirere@gmail.com>
2023-03-14 13:54:10 +0200

    Update all workflow

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const toggle = document.querySelector('.toggle');
 const body = document.querySelector('body');
 
 toggle.addEventListener('click', function () { 
-  body.classList.toggle('open')
+  body.classList.toggle('open');
 });
