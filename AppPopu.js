3b50eefcf3a2e020311308d7385c11b4edbabb07
massa <zoumaniguimassa12@gmail.com>
2023-03-15 10:24:59 +0000

    update fix error and added AppPopu.js popup-style.css

---
+++ AppPopu.js
@@ -0,0 +1,27 @@
+const seeBnt1 = document.querySelector('.see-project1');
+const popup1 = document.querySelector('.popup1');
+
+const seeBnt2 = document.querySelector('.see-project2');
+const popup2 = document.querySelector('.popup2');
+
+const seeBnt3 = document.querySelector('.see-project3');
+const popup3 = document.querySelector('.popup3');
+
+const seeBnt4 = document.querySelector('.see-project4');
+const popup4 = document.querySelector('.popup4');
+
+seeBnt1.addEventListener('click', () => {
+  popup1.style.display = 'block';
+});
+
+seeBnt2.addEventListener('click', () => {
+  popup2.style.display = 'block';
+});
+
+seeBnt3.addEventListener('click', () => {
+  popup3.style.display = 'block';
+});
+
+seeBnt4.addEventListener('click', () => {
+  popup4.style.display = 'block';
+});
