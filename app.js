2ca6995de433aafaf134888c49fe20f5e495a0dd
massa <zoumaniguimassa12@gmail.com>
2023-03-16 10:19:07 +0000

    update js to make an array to store cards of popup

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-
 const toggle = document.querySelector('.toggle');
 const body = document.querySelector('body');
 const menuOptions = document.querySelectorAll('.menu li');
-// const for bunton see project
-const seebnt1=document.getElementById('see-project1');
-const seebnt2=document.querySelector('.see-project2');
-const seebn3t=document.querySelector('.see-project3');
-const seebnt4=document.querySelector('.see-project4');
-
-// const for popup
-const popup1=document.querySelector('.popup1')
-const popup2=document.querySelector('.popup2')
-const popup3=document.querySelector('.popup3')
-const popup4=document.querySelector('.popup4')
-
-
-// addeventlistener for opening popup
-// seebnt1.addEventListener('click', () =>{
-//   popup1.style.display='block';
-//   console.log('hhhhhhh')
-// })
-
-
-seebnt1.addEventListener('click', () => {
-  console.log('jjh');
-})
 
 
 // methode open to take control on class toggle
 toggle.addEventListener('click', () => {
   body.classList.toggle('open');
 });
-
 
 // methode open to take control on class menu list
 menuOptions.forEach(option => {
