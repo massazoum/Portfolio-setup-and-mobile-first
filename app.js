ec8a8f2ca278082afef3f6af1573f9019443d26e
massa <zoumaniguimassa12@gmail.com>
2023-03-17 14:35:52 +0000

    creating local storage

--- app.js
+++ app.js
@@ -11,3 +11,39 @@ menuOptions.forEach((option) => {
     body.classList.remove('open');
   });
 });
+//    ----------------------------Local storage----------------------------------------------
+
+// Get references 
+const form = document.querySelector('.form0');
+const nameInput = document.querySelector('.inp-name');
+const emailInput = document.querySelector('.inp-email');
+const messageInput = document.querySelector('.inp-text');
+
+// Define
+function saveFormData() {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+// function to set
+// function loadFormData() {
+//   const savedFormData = localStorage.getItem('formData');
+//   if (savedFormData) {
+//     const formData = JSON.parse(savedFormData);
+//     nameInput.value = formData.name;
+//     emailInput.value = formData.email;
+//     messageInput.value = formData.message;
+//   }
+// }
+
+// // Add event listeners to the input fields
+// nameInput.addEventListener('input', saveFormData);
+// emailInput.addEventListener('input', saveFormData);
+// messageInput.addEventListener('input', saveFormData);
+
+// // Calling
+// loadFormData();
